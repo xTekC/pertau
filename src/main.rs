61e0d483a5cb92e148a7a5b37b84c3dc69ed0630
xTeKc <bcd3v@pm.me>
2022-10-01 22:21:23 -0400

    clear

--- src/main.rs
+++ src/main.rs
@@ -13,30 +13,4 @@ mod templates;
 use perseus::{Html, PerseusApp, PerseusRoot};
 use sycamore::prelude::view;
 
-pub fn main() {
-    main();
-}
-
-#[perseus::main(perseus_integration::dflt_server)]
-pub fn viz<G: Html>() -> PerseusApp<G> {
-    PerseusApp::new()
-        .template(crate::templates::index::get_template)
-        .template(crate::templates::long::get_template)
-        .error_pages(crate::error_pages::get_error_pages)
-        .index_view(|cx| {
-            view! { cx,
-                html {
-                    head {
-                        meta(charset = "UTF-8")
-                        meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
-                        // Perseus automatically resolves `/.perseus/static/` URLs to the contents of the `static/` directory at the project root
-                        link(rel = "stylesheet", href = ".perseus/static/style.css")
-                    }
-                    body {
-                        // Quirk: this creates a wrapper `<div>` around the root `<div>` by necessity
-                        PerseusRoot()
-                    }
-                }
-            }
-        })
-}
+pub fn main() {}

849549586b9139aaff79b9871ea5787f5f40ad43
xTeKc <bcd3v@pm.me>
2022-10-19 16:10:00 -0400

    change fn name

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,13 +5,13 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn from_tauri(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Tauri!", name)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![from_tauri])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
